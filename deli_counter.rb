d8703bd4864cf1a0283393788c72c6830bdfc26b
William Luck <luck.william.h@gmail.com>
2022-10-19 10:35:09 -0300

    commit tree eeb5a42bdf08c39ebe96b4a8ad400bab9f607429

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+require 'pry'
+
+example_array = ["billy", "jason", "emily"]
+
+def line(array)
+
+    if array.length == 0
+        puts 'The line is currently empty.'
+    else
+        starting_string = 'The line is currently:'
+        array.each_with_index do |element, index|
+            starting_string << " #{index + 1}. #{element}"
+        end
+        puts starting_string
+    end
+end
+
+def take_a_number(array, new_name)
+    array << new_name
+    puts "Welcome, #{new_name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts 'There is nobody waiting to be served!'
+    else
+        puts "Currently serving #{array[0]}."
+        array.delete_at(0)
+    end
+end
+
+binding.pry
+0
